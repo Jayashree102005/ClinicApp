cdcad69b6cf47b7a24c1e7cae593190a0331d790
Jayashree102005 <jayashreelife123@gmail.com>
2026-06-08 17:52:28 +0530

    Fix React Router refresh issue

--- my-project/Frontend/src/App.jsx
+++ my-project/Frontend/src/App.jsx
@@ -15,6 +15,7 @@ import DoctorDashboard from './pages/DoctorDashboard';
 import Services from './pages/Services'; 
 import AdminDashboard from './pages/AdminDashboard';
 import AdminRoute from './component/AdminRoute'; 
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   const location = useLocation();
