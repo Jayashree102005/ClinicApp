60cf4f594f55bfad4330542b33bd985e3cb2a105
Jayashree102005 <jayashreelife123@gmail.com>
2026-06-09 16:45:11 +0530

    Add forgot password and reset password functionality

--- my-project/Backend/server.js
+++ my-project/Backend/server.js
@@ -12,9 +12,13 @@ const app = express();
 
 // Middleware - UPDATED to allow both localhost ports (5173 and 5174)
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'], 
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://clinic-app-tan.vercel.app'
+    ],
     credentials: true
-})); 
+}));
 app.use(express.json()); 
 
 // Connect to MongoDB
